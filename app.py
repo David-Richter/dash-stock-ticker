b1c16b9c6a76ce21adb27613539fec6e6a63040a
David <david.thurnau@web.de>
2019-09-29 18:46:53 +0200

    disable CSRF

--- app.py
+++ app.py
@@ -19,7 +19,8 @@ USERNAME_PASSWORD_PAIRS = {
 app = dash.Dash(
     __name__,
     meta_tags=[{"name": "viewport",
-                "content": "width=device-width, initial-scale=1"}]
+                "content": "width=device-width, initial-scale=1"}],
+    csrf_protect=False
 )
 
 auth = dash_auth.BasicAuth(app, USERNAME_PASSWORD_PAIRS)
